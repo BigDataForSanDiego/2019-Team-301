b757a5af5dbd664180f475e474b3e5e6adaeeac0
Keith <Keith@iMac.attlocal.net>
2019-03-12 16:52:04 -0700

    add all files

---
+++ src/main/java/org/leagueofamazingprogrammers/voluntrack/DefaultController.java
@@ -0,0 +1,10 @@
+package org.leagueofamazingprogrammers.voluntrack;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DefaultController {
+	String test() {
+		return "index";
+	}
+}
